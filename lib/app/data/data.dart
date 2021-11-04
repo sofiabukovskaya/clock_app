8f9167a70675caca052af6e2c2d109754461b20a
sofiabukovskaya <sofi454545@gmail.com>
2021-11-04 18:29:46 +0200

    start impl alarm page

--- lib/app/data/data.dart
+++ lib/app/data/data.dart
@@ -1,4 +1,6 @@
+import 'package:clock_app/app/data/models/alarm_info.dart';
 import 'package:clock_app/app/data/models/menu_info.dart';
+import 'package:flutter/material.dart';
 
 import 'enums/menu_type_enum.dart';
 
@@ -11,4 +13,13 @@ List<MenuInfo> menuInfoItems = [
       title: 'Stopwatch', imageSource: 'assets/images/stopwatch_icon.png'),
   MenuInfo(MenuType.timer,
       title: 'Timer', imageSource: 'assets/images/timer_icon.png')
+];
+
+List<AlarmInfo> alarmInfoItems = [
+  AlarmInfo(DateTime.now().add(const Duration(hours: 1)),
+      description: 'Office',
+      gradientColor: [Colors.lightBlueAccent.shade400, Colors.purple.shade500]),
+  AlarmInfo(DateTime.now().add(const Duration(hours: 2)),
+      description: 'Sport',
+      gradientColor: [Colors.purple.shade600, Colors.red.shade500])
 ];
--- lib/app/pages/alarm_page.dart
+++ lib/app/pages/alarm_page.dart
@@ -1,3 +1,4 @@
+import 'package:clock_app/app/data/data.dart';
 import 'package:flutter/material.dart';
 
 class AlarmPage extends StatefulWidget {
@@ -10,14 +11,92 @@ class AlarmPage extends StatefulWidget {
 class _AlarmPageState extends State<AlarmPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const Text('Alarm',
-            style: TextStyle(
-                fontFamily: 'avenir', color: Colors.white, fontSize: 32)),
-        ListView()
-      ],
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 64, horizontal: 32),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text('Alarm',
+              style: TextStyle(
+                  fontFamily: 'avenir', color: Colors.white, fontSize: 32)),
+          Expanded(
+            child: ListView(
+                children: alarmInfoItems.map((alarm) {
+              return Container(
+                margin: const EdgeInsets.only(bottom: 32),
+                padding:
+                    const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        colors: alarm.gradientColor!,
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight),
+                    borderRadius: const BorderRadius.all(Radius.circular(24)),
+                    boxShadow: [
+                      BoxShadow(
+                          color: alarm.gradientColor!.last.withOpacity(0.4),
+                          blurRadius: 8,
+                          offset: const Offset(4, 4),
+                          spreadRadius: 4)
+                    ]),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            const Icon(Icons.label,
+                                color: Colors.white, size: 24),
+                            const SizedBox(width: 8),
+                            Text(
+                              alarm.description!,
+                              style: const TextStyle(
+                                  fontFamily: 'avenir', color: Colors.white),
+                            ),
+                          ],
+                        ),
+                        Switch(
+                          value: true,
+                          onChanged: (bool value) => {},
+                          activeColor: Colors.white,
+                        )
+                      ],
+                    ),
+                    const Text(
+                      'Mon-Fri',
+                      style:
+                          TextStyle(fontFamily: 'avenir', color: Colors.white),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text(
+                          '7:00 AM',
+                          style: TextStyle(
+                              fontFamily: 'avenir',
+                              color: Colors.white,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 24),
+                        ),
+                        Icon(Icons.keyboard_arrow_down,
+                            color: Colors.white, size: 36)
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            }).followedBy([
+              Container(
+                color: Colors.red,
+                height: 100,
+                width: 100,
+              )
+            ]).toList()),
+          )
+        ],
+      ),
     );
   }
 }
